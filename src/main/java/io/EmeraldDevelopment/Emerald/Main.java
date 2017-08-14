e73c88cc1beb3809555a20c11060562665a4fa59
Atrius <atriusedm@gmail.com>
2017-08-14 03:24:57 -0400

    Import repository

---
+++ src/main/java/io/EmeraldDevelopment/Emerald/Main.java
@@ -0,0 +1,12 @@
+package io.EmeraldDevelopment.Emerald;
+
+/** --- Authors ---
+ * @author Atrius
+ * @author Khriox
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
