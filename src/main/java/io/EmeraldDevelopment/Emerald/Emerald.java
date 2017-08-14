e06b72038e338a5bcb14c8063a67103a0f3d985e
Atrius <atriusedm@gmail.com>
2017-08-14 04:15:35 -0400

    Ghetto Heck

--- src/main/java/io/EmeraldDevelopment/Emerald/Emerald.java
+++ src/main/java/io/EmeraldDevelopment/Emerald/Emerald.java
@@ -3,6 +3,8 @@ package io.EmeraldDevelopment.Emerald;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.util.DiscordException;
 
+import java.io.File;
+
 /** --- Authors ---
  * @author Atrius
  * @author Khriox
@@ -12,8 +14,20 @@ public class Emerald {
     // Emerald Initialization
     public static void main(String[] args) {
 
+        // Create directories
+        createDirectories();
         // Login to the service
         login(args[0]);
+    }
+
+    // Create directories for users and guilds
+    private static void createDirectories() {
+
+        File users = new File("users/");
+        File guilds = new File("guilds/");
+
+        if (!users.exists()) users.mkdirs();
+        if (!guilds.exists()) guilds.mkdirs();
     }
 
     // Log into the service with the given token
