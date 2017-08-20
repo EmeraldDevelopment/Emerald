555dd50957be7b4aab1732b871d910e9ec45347d
Atrius <atriusedm@gmail.com>
2017-08-20 04:26:20 -0400

    Add internal commands

--- src/main/java/io/Emerald/commandmanager/internalcommands/HelpCommand.java
+++ src/main/java/io/Emerald/commandmanager/internalcommands/HelpCommand.java
@@ -1,4 +1,18 @@
 package io.Emerald.commandmanager.internalcommands;
 
-public class HelpCommand {
+import io.Emerald.annotations.CommandMeta;
+import io.Emerald.commandmanager.InternalCommand;
+import io.Emerald.internal.api.CommandSender;
+
+@CommandMeta(
+        command = "help",
+        usage = "!help",
+        description = "Displays all commands in the bot."
+)
+public class HelpCommand extends InternalCommand {
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+    }
 }
--- src/main/java/io/Emerald/commandmanager/internalcommands/StatsCommand.java
+++ src/main/java/io/Emerald/commandmanager/internalcommands/StatsCommand.java
@@ -1,4 +1,18 @@
 package io.Emerald.commandmanager.internalcommands;
 
-public class StatsCommand {
+import io.Emerald.annotations.CommandMeta;
+import io.Emerald.commandmanager.InternalCommand;
+import io.Emerald.internal.api.CommandSender;
+
+@CommandMeta(
+        command = "stats",
+        usage = "!stats",
+        description = "Displays some information on the bot."
+)
+public class StatsCommand extends InternalCommand {
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+    }
 }
